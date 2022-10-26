54a1907433445cbdeee7b84fdeb7652883f7cf94
faikarghas <ghassanfaikar@yahoo.com>
2022-10-27 04:04:17 +0700

    update fb controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\FacebookLoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\HomeController;
 use App\Http\Controllers\Web\GoogleLoginController;
+use App\Http\Controllers\Web\FacebookLoginController;
 use App\Http\Controllers\Admin\DasboardController;
+
 
 /*
 |--------------------------------------------------------------------------
