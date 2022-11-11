9bfd800818a5cfaf9a5bf51ea3bfcf863dd0f76b
faikarghas <ghassanfaikar@yahoo.com>
2022-11-11 16:03:15 +0700

    update banner

--- resources/views/web/pages/belanja.blade.php
+++ resources/views/web/pages/belanja.blade.php
@@ -11,7 +11,7 @@
             <div class="bg-[#FCF006] lg:bg-[#F8F8F8] px-12 pt-28 pb-28 flex flex-col items-center justify-center">
                 <h1 class="text-black text-[60px] font-head mb-8 leading-tight">TEMUKAN KAMI DI MARKET PLACE</h1>
                 <div class="flex flex-col lg:flex-row justify-center gap-12 w-full">
-                    <a class="bg-white w-[100%] lg:w-[244px] h-[192px] rounded-full rounded-[2rem] lg:rounded-3xl p-4 flex flex-row lg:flex-col justify-center items-center shadow-[7px_10px_18px_rgba(0,0,0,0.25)]">
+                    <a href="https://www.tokopedia.com/b7official/etalase/extra-joss?utm_source=website&utm_medium=brand&utm_campaign=_eta_ej_web_031112_311222" target="_blank" rel="noopener" class="bg-white w-[100%] lg:w-[244px] h-[192px] rounded-full rounded-[2rem] lg:rounded-3xl p-4 flex flex-row lg:flex-col justify-center items-center shadow-[7px_10px_18px_rgba(0,0,0,0.25)]">
                         <div class="w-[180px] flex flex-row lg:flex-col justify-left items-center">
                             <img
                             alt="image-femmy"
@@ -21,7 +21,7 @@
                             <span class="font-sans text-femmy-pdark">Tokopedia</span>
                         </div>
                     </a>
-                    <a class="bg-white w-[100%] lg:w-[244px] h-[192px] rounded-full rounded-[2rem] lg:rounded-3xl p-4 flex flex-row lg:flex-col justify-center items-center shadow-[7px_10px_18px_rgba(0,0,0,0.25)]">
+                    <a href="" target="_blank" rel="noopener" class="bg-white w-[100%] lg:w-[244px] h-[192px] rounded-full rounded-[2rem] lg:rounded-3xl p-4 flex flex-row lg:flex-col justify-center items-center shadow-[7px_10px_18px_rgba(0,0,0,0.25)]">
                         <div class="w-[180px] flex flex-row lg:flex-col justify-left items-center">
                             <img
                             alt="image-femmy"
@@ -31,7 +31,7 @@
                             <span class="font-sans text-femmy-pdark">Lazada</span>
                         </div>
                     </a>
-                    <a class="bg-white w-[100%] lg:w-[244px] h-[192px] rounded-full rounded-[2rem] lg:rounded-3xl p-4 flex flex-row lg:flex-col justify-center items-center shadow-[7px_10px_18px_rgba(0,0,0,0.25)]">
+                    <a href="https://shopee.co.id/universal-link/bintangtoedjoe_officialstore?shopCollection=39397425&deep_and_web=1&utm_campaign=s156508041_ss_id_ig00_etalaseej&utm_source=instagram&utm_medium=seller&utm_content=&smtt=9#product_list" target="_blank" rel="noopener" class="bg-white w-[100%] lg:w-[244px] h-[192px] rounded-full rounded-[2rem] lg:rounded-3xl p-4 flex flex-row lg:flex-col justify-center items-center shadow-[7px_10px_18px_rgba(0,0,0,0.25)]">
                         <div class="w-[180px] flex flex-row lg:flex-col justify-left items-center">
                             <img
                             alt="image-femmy"
@@ -41,7 +41,7 @@
                             <span class="font-sans text-femmy-pdark">Shopee</span>
                         </div>
                     </a>
-                    <a class="bg-white w-[100%] lg:w-[244px] h-[192px] rounded-full rounded-[2rem] lg:rounded-3xl p-4 flex flex-row lg:flex-col justify-center items-center shadow-[7px_10px_18px_rgba(0,0,0,0.25)]">
+                    <a href="" target="_blank" rel="noopener" class="bg-white w-[100%] lg:w-[244px] h-[192px] rounded-full rounded-[2rem] lg:rounded-3xl p-4 flex flex-row lg:flex-col justify-center items-center shadow-[7px_10px_18px_rgba(0,0,0,0.25)]">
                         <div class="w-[180px] flex flex-row lg:flex-col justify-left items-center">
                             <img
                             alt="image-femmy"
