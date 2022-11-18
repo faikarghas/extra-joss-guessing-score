a8c7f6bcc7dabfdcc2369d351eb82bf5007f9c4d
faikarghas <ghassanfaikar@yahoo.com>
2022-11-18 15:53:37 +0700

    fix role amin

--- resources/views/admin/layouts/_dashboard/sidebar.blade.php
+++ resources/views/admin/layouts/_dashboard/sidebar.blade.php
@@ -39,83 +39,83 @@
     <li class="menu-header small text-uppercase">
        <span class="menu-header-text">Pages</span>
     </li>
-    @if (!Auth::user()->email == 'syahril.anwar@b7leap.com')
-    <!-- Pages -->
-    <li class="menu-item {{ set_active(['posts.index','posts.create', 'posts.edit']) }} {{ set_open(['posts.index','posts.create', 'posts.edit']) }} ">
-      <a href="javascript:void(0);" class="menu-link menu-toggle">
-        <i class="menu-icon tf-icons bx bx-news"></i>
-        <div data-i18n="Layouts">Posts</div>
-      </a>
-      <ul class="menu-sub">
-        <li class="menu-item {{ set_active(['posts.index']) }} ">
-          <a href="{{ route('posts.index') }}" class="menu-link">
-            <div data-i18n="Without menu">List</div>
-          </a>
-        </li>
-        <li class="menu-item {{ set_active(['posts.create']) }}">
-          <a href="{{ route('posts.create') }}" class="menu-link">
-            <div data-i18n="Without navbar">Create</div>
-          </a>
-        </li>
-      </ul>
-    </li>
+    @if (Auth::user()->email == 'syahril.anwar@b7leap.com')
+    @else
+      <li class="menu-item {{ set_active(['posts.index','posts.create', 'posts.edit']) }} {{ set_open(['posts.index','posts.create', 'posts.edit']) }} ">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class="menu-icon tf-icons bx bx-news"></i>
+          <div data-i18n="Layouts">Posts</div>
+        </a>
+        <ul class="menu-sub">
+          <li class="menu-item {{ set_active(['posts.index']) }} ">
+            <a href="{{ route('posts.index') }}" class="menu-link">
+              <div data-i18n="Without menu">List</div>
+            </a>
+          </li>
+          <li class="menu-item {{ set_active(['posts.create']) }}">
+            <a href="{{ route('posts.create') }}" class="menu-link">
+              <div data-i18n="Without navbar">Create</div>
+            </a>
+          </li>
+        </ul>
+      </li>
     @endif
 
-    @if (!Auth::user()->email == 'syahril.anwar@b7leap.com')
-     <!-- Components -->
-     <li class="menu-header small text-uppercase"><span class="menu-header-text">Games / Quiz</span></li>
-     <li class="menu-item {{ set_active(['teams.index','teams.edit','matchs.create', 'matchs.edit']) }} {{ set_open(['teams.index','matchs.index', 'teams.edit', 'matchs.create','rounds.index']) }} ">
-      <a href="javascript:void(0);" class="menu-link menu-toggle">
-        <i class="menu-icon tf-icons bx bx-football"></i>
-        <div data-i18n="Layouts">Footbals</div>
-      </a>
-      <ul class="menu-sub">
-        <li class="menu-item {{ set_active(['teams.index','teams.edit']) }} ">
-          <a href="{{ route('teams.index') }}" class="menu-link">
-            <div data-i18n="Without menu">List Teams</div>
-          </a>
+    @if (Auth::user()->email == 'syahril.anwar@b7leap.com')
+    @else
+      <li class="menu-header small text-uppercase"><span class="menu-header-text">Games / Quiz</span></li>
+      <li class="menu-item {{ set_active(['teams.index','teams.edit','matchs.create', 'matchs.edit']) }} {{ set_open(['teams.index','matchs.index', 'teams.edit', 'matchs.create','rounds.index']) }} ">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class="menu-icon tf-icons bx bx-football"></i>
+          <div data-i18n="Layouts">Footbals</div>
+        </a>
+        <ul class="menu-sub">
+          <li class="menu-item {{ set_active(['teams.index','teams.edit']) }} ">
+            <a href="{{ route('teams.index') }}" class="menu-link">
+              <div data-i18n="Without menu">List Teams</div>
+            </a>
+          </li>
+          <li class="menu-item {{ set_active(['matchs.index']) }}">
+            <a href="{{ route('matchs.index') }}" class="menu-link">
+              <div data-i18n="Without navbar">List Matches</div>
+            </a>
+          </li>
+          <li class="menu-item {{ set_active(['rounds.index']) }}">
+            <a href="{{ route('rounds.index') }}" class="menu-link">
+              <div data-i18n="Without navbar">List Rounds</div>
+            </a>
+          </li>
+          <li class="menu-item {{ set_active(['matchs.create']) }}">
+            <a href="{{ route('matchs.create') }}" class="menu-link">
+              <div data-i18n="Without navbar">Create Matches</div>
+            </a>
+          </li>
+        </ul>
         </li>
-        <li class="menu-item {{ set_active(['matchs.index']) }}">
-          <a href="{{ route('matchs.index') }}" class="menu-link">
-            <div data-i18n="Without navbar">List Matches</div>
-          </a>
-        </li>
-        <li class="menu-item {{ set_active(['rounds.index']) }}">
-          <a href="{{ route('rounds.index') }}" class="menu-link">
-            <div data-i18n="Without navbar">List Rounds</div>
-          </a>
-        </li>
-        <li class="menu-item {{ set_active(['matchs.create']) }}">
-          <a href="{{ route('matchs.create') }}" class="menu-link">
-            <div data-i18n="Without navbar">Create Matches</div>
-          </a>
-        </li>
-      </ul>
-    </li>
 
-    <li class="menu-item {{ set_active(['quizs.index','quizs.create', 'quizs.edit', 'quizchoices.create']) }} {{ set_open(['quizs.index','quizs.create','quizchoices.create']) }}">
-      <a href="javascript:void(0);" class="menu-link menu-toggle">
-        <i class="menu-icon tf-icons bx bxs-message-check"></i>
-        <div data-i18n="Layouts">Quiz</div>
-      </a>
-      <ul class="menu-sub">
-        <li class="menu-item {{ set_active(['quizs.index']) }}">
-          <a href="{{ route('quizs.index') }}" class="menu-link">
-            <div data-i18n="Without menu">List Quiz</div>
-          </a>
-        </li>
-        <li class="menu-item  {{ set_active(['quizs.create']) }}">
-          <a href="{{ route('quizs.create') }}" class="menu-link">
-            <div data-i18n="Without navbar">Create Quiz</div>
-          </a>
-        </li>
-        <li class="menu-item  {{ set_active(['quizchoices.create']) }}">
-          <a href="{{ route('quizchoices.create') }}" class="menu-link">
-            <div data-i18n="Without navbar">Create Quiz Option</div>
-          </a>
-        </li>
-      </ul>
-    </li>
+      <li class="menu-item {{ set_active(['quizs.index','quizs.create', 'quizs.edit', 'quizchoices.create']) }} {{ set_open(['quizs.index','quizs.create','quizchoices.create']) }}">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class="menu-icon tf-icons bx bxs-message-check"></i>
+          <div data-i18n="Layouts">Quiz</div>
+        </a>
+        <ul class="menu-sub">
+          <li class="menu-item {{ set_active(['quizs.index']) }}">
+            <a href="{{ route('quizs.index') }}" class="menu-link">
+              <div data-i18n="Without menu">List Quiz</div>
+            </a>
+          </li>
+          <li class="menu-item  {{ set_active(['quizs.create']) }}">
+            <a href="{{ route('quizs.create') }}" class="menu-link">
+              <div data-i18n="Without navbar">Create Quiz</div>
+            </a>
+          </li>
+          <li class="menu-item  {{ set_active(['quizchoices.create']) }}">
+            <a href="{{ route('quizchoices.create') }}" class="menu-link">
+              <div data-i18n="Without navbar">Create Quiz Option</div>
+            </a>
+          </li>
+        </ul>
+      </li>
     @endif
 
     <li class="menu-header small text-uppercase">
